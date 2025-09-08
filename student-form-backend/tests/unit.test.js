d7f2eecc8ab39c8aa7072eb24bb07be1640ccf8d
Spandana0002 <spandanayelugu@gmail.com>
2025-09-08 15:54:24 +0530

    Update unit.test.js

--- student-form-backend/tests/unit.test.js
+++ student-form-backend/tests/unit.test.js
@@ -1,6 +1,6 @@
 // Example unit test for validating email format
 function isValidEmail(email) {
-  return /^[a-zA-Z0-9._%+-]+@}$/.test(email);
+  return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$/.test(email);
 }
 
 test("Valid email should pass", () => {
